b5680a78d6d54538e88e70d9a77da64392f029a6
arrahmanever <arrahmanever@gmail.com>
2021-07-29 05:57:56 +0000

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,17 +12,17 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Devops-nextgen"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "Terraform-integration-GitHub-Actions"
     }
   }
 }
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 
